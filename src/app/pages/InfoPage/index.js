8a9fc35c46bbb2c3647ae7d20d2b8c7a559157be
tracycollins <tc@threeceemedia.com>
2021-04-27 18:25:10 -0400

    package

--- src/app/pages/InfoPage/index.js
+++ src/app/pages/InfoPage/index.js
@@ -3,8 +3,6 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Markdown from 'react-markdown';
 
-// import TextField from '@material-ui/core/TextField';
-
 import {
   makeStyles,
   createMuiTheme,
@@ -13,7 +11,6 @@ import {
 } from '@material-ui/core/styles';
 
 import '@fontsource/bungee'; // Defaults to weight 400.
-// import { title, text } from './info.json';
 import { title, text } from './info_txt.js';
 
 let themeBungee = createMuiTheme({
@@ -34,7 +31,6 @@ themeBungee = responsiveFontSizes(themeBungee);
 
 let themeText = createMuiTheme({
   typography: {
-    // fontFamily: ['Bungee'].join(','),
     h1: {
       fontSize: 120,
     },
@@ -52,9 +48,6 @@ const useStyles = makeStyles(theme => ({
   root: {
     width: '100%',
     height: 'auto',
-    // display: 'flex',
-    // marginTop: '20%',
-    // flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -69,15 +62,11 @@ const useStyles = makeStyles(theme => ({
     width: '70%',
     padding: theme.spacing(1),
     marginLeft: 'auto',
-    // justifyContent: 'center',
-    // textAlign: 'center',
   },
 }));
 
 export function InfoPage() {
   const classes = useStyles();
-  // var src = '# Load the markdown document';
-
   return (
     <ThemeProvider theme={themeText}>
       <Paper elevation={0} className={classes.root}>
